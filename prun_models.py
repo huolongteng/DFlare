3f0ff65b0d08fd06166859c5be1aa238c3b6c86a
HUO LONGTENG-Leo <hlt@bistu.edu.cn>
2026-04-22 17:28:24 +0800

    Fix compressed model eval/calibration data splits

--- prun_models.py
+++ prun_models.py
@@ -1,7 +1,5 @@
 import os
 import torch
-import torch.nn as nn
-import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
@@ -48,9 +46,7 @@ final_pruning_accuracies = {}
 
 print("\n--- Processing MNIST Pruning ---")
 transform_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-mnist_train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform_mnist)
 mnist_test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform_mnist)
-mnist_train_loader = DataLoader(mnist_train_dataset, batch_size=64, shuffle=True)
 mnist_test_loader = DataLoader(mnist_test_dataset, batch_size=1000, shuffle=False)
 
 for model_name, model in original_models_mnist.items():
@@ -102,9 +98,7 @@ transform_cifar = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
-cifar_train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_cifar)
 cifar_test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_cifar)
-cifar_train_loader = DataLoader(cifar_train_dataset, batch_size=128, shuffle=True)
 cifar_test_loader = DataLoader(cifar_test_dataset, batch_size=100, shuffle=False)
 
 for model_name, model in original_models_cifar.items():
